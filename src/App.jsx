e1cdf51449320250af8a473137c12c67161eb722
Dantomaz <daniel.t.mazurczak@gmail.com>
2024-09-12 11:23:05 +0200

    implement card dealing animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,11 @@
+import { useGSAP } from "@gsap/react";
+import { gsap } from "gsap";
+import { Flip } from "gsap/Flip";
 import { Provider } from "react-redux";
 import Routing from "./features/routing/Routing";
 import store from "./store";
+
+gsap.registerPlugin(useGSAP, Flip);
 
 const App = () => {
   return (
--- src/features/board/card/Card.jsx
+++ src/features/board/card/Card.jsx
@@ -1,4 +1,7 @@
-import React from "react";
+import { useGSAP } from "@gsap/react";
+import { gsap } from "gsap";
+import { Flip } from "gsap/Flip";
+import React, { useLayoutEffect, useRef } from "react";
 import { useSelector } from "react-redux";
 import { preventDefaultAction } from "../../../global/utils";
 import ImageModal from "../../image-modal/ImageModal";
@@ -21,10 +24,36 @@ const Card = ({ number, image }) => {
     hideImagePreview,
   } = useCard({ number });
   const gameState = useSelector((state) => state.gameStateManager.gameState);
+  const cardRef = useRef();
+  const flipState = useRef();
+
+  useLayoutEffect(() => {
+    flipState.current = Flip.getState(cardRef.current);
+
+    const deltaX = window.innerWidth / 2 - flipState.current.elementStates[0].bounds.left - flipState.current.elementStates[0].bounds.width / 2;
+    const deltaY = -flipState.current.elementStates[0].bounds.bottom - 100;
+
+    // offset the card before rendering to prepare for the animation
+    gsap.set(cardRef.current, { transform: `translate(${deltaX}px, ${deltaY}px)` });
+  }, []);
+
+  useGSAP(() => {
+    // animate the card movement
+    Flip.to(flipState.current, {
+      ease: "elastic.out(1, 0.75)",
+      duration: 0.5,
+      delay: 1 + Math.random() * 1.2,
+      onComplete: () => {
+        // Ensure the card becomes part of the normal layout after the animation
+        gsap.set(cardRef.current, { clearProps: "all" });
+      },
+    });
+  });
 
   return (
     <>
       <div
+        ref={cardRef}
         className={`${styles["card"]} ${closed && styles["card-closed"]} ${
           styles[isHighlightedBlue ? "highlighted-blue" : isHighlightedRed ? "highlighted-red" : ""]
         }`}
