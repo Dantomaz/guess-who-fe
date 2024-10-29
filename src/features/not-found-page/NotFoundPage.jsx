3c15d8042966ceea53fe55d9434746c1644ff092
Dantomaz <daniel.t.mazurczak@gmail.com>
2024-10-29 15:31:32 +0100

    remove redirecting when page not found (reconnect provider handles this case)

--- src/features/not-found-page/NotFoundPage.jsx
+++ src/features/not-found-page/NotFoundPage.jsx
@@ -1,26 +1,11 @@
-import { useEffect } from "react";
-import { useNavigate } from "react-router-dom";
 import "../../global/styles/classes.scss";
 import styles from "./NotFoundPage.module.scss";
 
 const NotFoundPage = () => {
-  const navigate = useNavigate();
-
-  const redirect = () => {
-    navigate("/", { replace: true });
-  };
-
-  useEffect(() => {
-    setTimeout(() => {
-      redirect();
-    }, 3000);
-  });
-
   return (
     <div className={styles["page-background"]}>
       <div className={styles["page-text"]}>
         <p className={styles["title"]}>404 Page Not Found</p>
-        <p className={styles["text"]}>Redirecting to title page...</p>
       </div>
     </div>
   );
