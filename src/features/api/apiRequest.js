6f2bb81662469a150f7f7a14402b16b57ece5d1f
Dantomaz <daniel.t.mazurczak@gmail.com>
2024-12-10 14:22:40 +0100

    file formatting

--- src/features/api/apiRequest.js
+++ src/features/api/apiRequest.js
@@ -20,36 +20,63 @@ import {
 } from "./web-socket/apiWsEndpoints";
 import { publish, subscribe, unsubscribe } from "./web-socket/stompClient";
 
+// ===== headers ===== /
+
 const imageHeaders = { headers: { "Content-Type": "multipart/form-data", Accept: "application/json" } };
+
+// ===== error ===== /
 
 export const subscribeQueueError = ({ callback }) => subscribe(QUEUE_ERROR, callback);
 
+// ===== player ===== /
+
 export const requestPlayerCreate = ({ nickname }) => axiosPost(insertVariables(PLAYER_CREATE, nickname));
+
 export const subscribeTopicPlayers = ({ roomId, callback }) => subscribe(insertVariables(TOPIC_PLAYERS, roomId), callback);
+
 export const publishPlayerChangeName = ({ roomId, playerId, newName }) =>
   publish(insertVariables(APP_PLAYER_CHANGE_NAME, roomId, playerId), { payload: newName }); // every String has to be named 'payload' when sending over STOMP
+
 export const publishPlayerChangeTeam = ({ roomId, playerId, newTeam }) => publish(insertVariables(APP_PLAYER_CHANGE_TEAM, roomId, playerId), newTeam);
+
 export const publishPlayerMakeHost = ({ roomId, playerId }) => publish(insertVariables(APP_PLAYER_MAKE_HOST, roomId, playerId));
+
 export const publishPlayerKick = ({ roomId, playerId }) => publish(insertVariables(APP_PLAYER_KICK, roomId, playerId));
 
+// ===== room ===== /
+
 export const requestRoomCreate = ({ player }) => axiosPost(ROOM_CREATE, player);
+
 export const requestRoomJoin = ({ roomId, player }) => axiosPost(insertVariables(ROOM_JOIN, roomId), player);
+
 export const requestRoomLeave = ({ roomId, playerId }) => axiosDelete(insertVariables(ROOM_LEAVE, roomId, playerId));
+
 export const requestRoomReconnect = () => axiosPost(ROOM_RECONNECT);
 
+// ===== images ===== /
+
 export const requestImagesUpload = ({ roomId, formData }) => axiosPost(insertVariables(IMAGES_UPLOAD, roomId), formData, imageHeaders);
+
 export const subscribeTopicImages = ({ roomId, callback }) => subscribe(insertVariables(TOPIC_IMAGES, roomId), callback);
 
+// ===== gamestate ===== /
+
 export const subscribeTopicGameState = ({ roomId, team, callback }) => subscribe(insertVariables(TOPIC_GAME_STATE, roomId, team), callback);
+
 export const unsubscribeTopicGameState = ({ roomId, team }) => unsubscribe(insertVariables(TOPIC_GAME_STATE, roomId, team));
 
 export const publishGameRestart = ({ roomId }) => publish(insertVariables(APP_GAME_RESTART, roomId));
 
 export const publishGamePrepare = ({ roomId, useDefaultImages }) => publish(insertVariables(APP_GAME_PREPARE, roomId), useDefaultImages);
+
 export const publishVoteForCard = ({ roomId, playerId, cardNumber }) => publish(insertVariables(APP_VOTE_FOR_CARD, roomId), { playerId, cardNumber });
 
 export const publishEndTurn = ({ roomId }) => publish(insertVariables(APP_END_TURN, roomId));
+
 export const publishToggleCard = ({ roomId, cardNumber, team }) => publish(insertVariables(APP_TOGGLE_CARD, roomId), { cardNumber, team });
+
 export const publishGuessCard = ({ roomId, cardNumber }) => publish(insertVariables(APP_GUESS_CARD, roomId), cardNumber);
+
+// ===== disconnect ===== /
 
 export const subscribeTopicDisconnect = ({ roomId, playerId, callback }) => subscribe(insertVariables(TOPIC_DISCONNECT, roomId, playerId), callback);
