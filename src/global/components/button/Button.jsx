4675788325daa704c545f8c4f1fd34f18374caad
Dantomaz <daniel.t.mazurczak@gmail.com>
2024-12-16 13:54:46 +0100

    little visual tweaks

--- src/global/components/button/Button.jsx
+++ src/global/components/button/Button.jsx
@@ -8,12 +8,9 @@ const Button = forwardRef(({ children, className, overrideClasses, disabled, ena
   const defaultClasses = `${styles["button"]} ${isDisabled && styles["button-disabled"]} ${className || ""}`;
 
   return (
-    // Wrapper is needed to stop button from growing to fill available space
-    <div className={styles["button-wrapper"]}>
-      <button ref={ref} className={overrideClasses ? className : defaultClasses} {...rest} disabled={isDisabled}>
-        {children}
-      </button>
-    </div>
+    <button ref={ref} className={overrideClasses ? className : defaultClasses} {...rest} disabled={isDisabled}>
+      {children}
+    </button>
   );
 });
 
